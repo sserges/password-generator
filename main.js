62368676a1bf6abafaba315b7e32aa31ea23e3c9
sserges <agounkeserge@gmail.com>
2022-11-21 18:34:11 -0400

    getRandomNumber and getRandomSymbol functions added

--- main.js
+++ main.js
@@ -8,4 +8,13 @@ function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 
-console.log(getRandomUpper())
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(getRandomSymbol())
